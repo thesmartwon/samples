2cdd13765bae35c8b72083ca808fb21b519ad499
thesmartwon <thesmartwon@pm.me>
2024-01-05 20:51:21 -0500

    fix indexer again

--- indexer.mjs
+++ indexer.mjs
@@ -1,5 +1,5 @@
 import { join } from 'node:path';
-import { readdirSync, statSync, writeFileSync  } from 'node:fs';
+import { existsSync, readdirSync, statSync, writeFileSync  } from 'node:fs';
 
 const stripExt = f => f.replace(/\..*/, '');
 const isSound = f => ['ogg', 'm4a', 'wav'].some(ext => f.endsWith(ext));
@@ -14,7 +14,7 @@ const index = {
 };
 
 Object.keys(index).forEach(folder => {
-	if (!statSync(folder).isDirectory()) return;
+	if (!existsSync(folder) || !statSync(folder).isDirectory()) return;
 	readdirSync(folder).forEach(p => {
 		const subFolder = join(folder, p);
 		if (statSync(subFolder).isDirectory()) {

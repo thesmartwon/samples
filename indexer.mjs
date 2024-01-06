de1953f8ecc280ed7eee6c93b016656568a01569
thesmartwon <thesmartwon@pm.me>
2024-01-05 19:00:41 -0500

    don't index non-sound files

--- indexer.mjs
+++ indexer.mjs
@@ -2,6 +2,7 @@ import { join } from 'node:path';
 import { readdirSync, statSync, writeFileSync  } from 'node:fs';
 
 const stripExt = f => f.replace(/\..*/, '');
+const isSound = f => ['ogg', 'm4a', 'wav'].some(ext => f.endsWith(ext));
 
 const index = {
 	'percussion': {
@@ -14,7 +15,9 @@ const index = {
 Object.keys(index).forEach(folder => {
 	readdirSync(folder).forEach(p => {
 		const subFolder = join(folder, p);
-		if (statSync(subFolder).isDirectory()) index[folder][p] = readdirSync(subFolder).map(stripExt);
+		if (statSync(subFolder).isDirectory()) {
+			index[folder][p] = readdirSync(subFolder).filter(isSound).map(stripExt);
+		}
 	});
 });
 
